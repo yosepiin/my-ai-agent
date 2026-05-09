b9016787af378db773cb3d4889409873bec2671f
Ari Yosefin <zeth112345@gmail.com>
2026-05-10 02:02:33 +0800

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,8 @@
+export const metadata = { title: "My AI Agent" };
+export default function RootLayout({ children }) {
+  return (
+    <html lang="id">
+      <body>{children}</body>
+    </html>
+  );
+}
